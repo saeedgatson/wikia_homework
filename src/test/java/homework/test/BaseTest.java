1dcaac0adeed40e59aff160cc14d0e8040d43f53
Saeed Gatson <saeedgatson@gmail.com>
2014-06-18 22:12:35 -0700

    Driver Browser Selection Based On Properties File

--- src/test/java/homework/test/BaseTest.java
+++ src/test/java/homework/test/BaseTest.java
@@ -1,13 +1,18 @@
 package homework.test;
 
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
-  public String baseURL = "http://testhomework.wikia.com/";
-  public WebDriver driver = null;
+  private String browser = "firefox";
+  private String pathToDriver;
+  private String baseURL = "http://testhomework.wikia.com/";
+  protected WebDriver driver = null;
   
   public void beforeTest() {
 	  driver = getWebDriver();
@@ -19,8 +24,9 @@ public class BaseTest {
   }
   
   private WebDriver getWebDriver() {
-	  if(System.getProperty("browser").equals("chrome")) {
-          System.setProperty("webdriver.chrome.driver", "/pathto/my/chromedriver.ex");
+	  loadProperties();
+	  if(browser.equals("chrome")) {
+          System.setProperty("webdriver.chrome.driver", pathToDriver);
           driver = new ChromeDriver();
       } else {
           driver = new FirefoxDriver();
@@ -28,5 +34,18 @@ public class BaseTest {
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       driver.manage().window().maximize();
       return driver;
+  }
+  
+  private void loadProperties() {
+	  Properties props = new Properties();
+	  try {
+		  InputStream inputStream = ClassLoader.getSystemResourceAsStream("TestProps.properties");
+		  props.load(inputStream);
+		  browser = props.getProperty("TestProps.browser");
+		  pathToDriver = props.getProperty("TestProps.pathToDriver");
+	  }
+	  catch (Exception ex) {
+		  ex.printStackTrace();
+	  }
   }
 }
